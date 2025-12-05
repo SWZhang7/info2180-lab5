de68ca3704822bd5766298257b2fac096472ac2a
Selcon Zhang <selconwzhang@gmail.com>
2025-12-05 13:03:21 -0500

    Completed Exercise 4

--- world.php
+++ world.php
@@ -4,21 +4,48 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$country = isset($_GET['country']) ? trim($_GET['country']) : '';
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
+$country = isset($_GET['country']) ? $_GET['country'] : '';
+
 if ($country !== '') {
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    $stmt = $conn->prepare(
+        "SELECT name, continent, independence_year, head_of_state
+         FROM countries
+         WHERE name LIKE :country"
+    );
     $stmt->execute(['country' => "%$country%"]);
 } else {
-    $stmt = $conn->query("SELECT * FROM countries");
+    $stmt = $conn->query(
+        "SELECT name, continent, independence_year, head_of_state
+         FROM countries"
+    );
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= htmlspecialchars($row['name']) . ' is ruled by ' . htmlspecialchars($row['head_of_state']); ?></li>
-<?php endforeach; ?>
-</ul>
+
+<?php if (count($results) > 0): ?>
+<table id="countries">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year']) ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php else: ?>
+<p>No results found.</p>
+<?php endif; ?>
